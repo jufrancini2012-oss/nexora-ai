2ab4439ebe3875c2ac89e197585c6f531f248b90
jufrancini2012-oss <ju.francini2012@gmail.com>
2026-09-25 14:26:15 -0300

    Corrigir sintaxe do worker de microparcerias para liberar publicação

--- apps/api/src/micro-partnership-worker.js
+++ apps/api/src/micro-partnership-worker.js
@@ -4,7 +4,7 @@ import { handlePartnerRedirect, loadMicroPartnerships, runMicroPartnershipCycle 
 import { parseSmartCatalog, saveSmartProducts, loadSmartProducts } from './smart-catalog.js';
 
 function json(data,status=200){
-  return new Response(JSON.stringify(data),{status,headers:{'content-type':'application/json; charset=utf-8','cache-control':'no-store','access-control-allow-origin':'*'});
+  return new Response(JSON.stringify(data),{status,headers:{'content-type':'application/json; charset=utf-8','cache-control':'no-store','access-control-allow-origin':'*'}});
 }
 
 export default {
